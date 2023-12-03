5a4a2b04867886285f5f02975152ac6594b7cf4b
Ali Soliman <alisolimanworks@gmail.com>
2023-12-03 18:31:31 +0200

    removed ninja module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NinjasModule } from './ninjas/ninjas.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [NinjasModule, UserModule, AuthModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
