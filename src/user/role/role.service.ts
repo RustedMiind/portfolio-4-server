8323a386c825a391796045eaaff75f2c772a61d6
Ali Soliman <alisolimanworks@gmail.com>
2023-12-22 21:55:29 +0200

    assign role

--- src/user/role/role.service.ts
+++ src/user/role/role.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getRole(roleId: string) {
+    try {
+      const role = await this.prisma.role.findUnique({ where: { id: roleId } });
+      return role;
+    } catch (error) {
+      throw new BadRequestException();
+    }
+  }
+
+  async getRoles() {
+    try {
+      const roles = await this.prisma.role.findMany();
+      return roles;
+    } catch (error) {
+      throw new BadRequestException();
+    }
+  }
+
+  async createRoleWithPermissions(name: string, permissions: string[]) {
+    const rolesWithPermissions = permissions.map((permissionId) => ({
+      id: permissionId,
+    }));
+
+    try {
+      const createdRole = await this.prisma.role.create({
+        data: {
+          name,
+          permissions: {
+            connect: rolesWithPermissions,
+          },
+        },
+      });
+      return createdRole;
+    } catch (error) {
+      throw new Error(`Error creating role: ${error.message}`);
+    }
+  }
+
+  async createRole(name: string) {
+    try {
+      const createdRole = await this.prisma.role.create({
+        data: {
+          name,
+        },
+      });
+      return createdRole;
+    } catch (error) {
+      throw new Error(`Error creating role: ${error.message}`);
+    }
+  }
+
+  async assignRole(userId: string, roleId: string) {
+    try {
+      const user = await this.prisma.user.update({
+        where: { id: userId },
+        data: { role: { connect: { id: roleId } } },
+      });
+      return user;
+    } catch (error) {
+      throw new BadRequestException();
+    }
+  }
+}
