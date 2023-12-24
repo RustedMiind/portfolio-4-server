72dd262a85335cd23e1853bb1c376b7c26fa395a
Ali Soliman <alisolimanworks@gmail.com>
2023-12-24 20:32:37 +0200

    New Permissions

--- src/user/permission/permission.enum.ts
+++ src/user/permission/permission.enum.ts
@@ -1,3 +1,9 @@
 export enum PermissionName {
   MANAGE_ROLE = 'manage_role',
+  DELETE_ROLE = 'delete_role',
+  ASSIGN_ROLE = 'assign_role',
+  UPDATE_USER_PASSWORD = 'update_user_password',
+  CREATE_PRODUCT = 'create_product',
+  UPDATE_PRODUCT = 'update_product',
+  DELETE_PRODUCT = 'delete_product',
 }
