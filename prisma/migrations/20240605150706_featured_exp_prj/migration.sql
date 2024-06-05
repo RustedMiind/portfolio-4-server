07b9ae3d5d58078c41f6d8ba7ca03c373bc04fa9
rustedmiind <alisolimanworks@gmail.com>
2024-06-05 18:15:09 +0300

    migration for featured attr

---
+++ prisma/migrations/20240605150706_featured_exp_prj/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Experience" ADD COLUMN     "featured" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "featured" BOOLEAN NOT NULL DEFAULT false;
